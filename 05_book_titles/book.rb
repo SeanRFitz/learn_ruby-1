2aa244afb324fc8ef6f30980b5d7098651387f3d
Sean Fitzgerald <Sean.R.Fitzgerald@outlook.com>
2017-05-30 20:51:47 -0500

    Passed the book titles tests

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+	def title
+		words = @title.split.map do |word|
+			if "and or the over in the of a an".split.include?(word)
+				word
+			else
+				word.capitalize
+			end
+		end
+	words.first.capitalize!
+	@title = words.join(" ")
+end
+
+
 end
