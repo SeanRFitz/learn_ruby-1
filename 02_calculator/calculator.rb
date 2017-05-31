8b22816518c4d637d3586c1f9c86b02c8cc7f399
Sean Fitzgerald <Sean.R.Fitzgerald@outlook.com>
2017-05-30 20:47:25 -0500

    Passed the calculator tests and created my own tests [previous commits were corrupted. I am commiting the final version of these files]

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,30 @@
-#write your code here
+def add(num1, num2)
+	num1 + num2
+end
+
+def subtract(num1, num2)
+	num1 - num2
+end
+
+def sum(array)
+	total = 0
+	array.each {|num| total += num}
+	total
+end
+
+def multiply(array)
+	total = 1
+	array.each {|num| total *= num}
+	total
+end
+
+def power(num, exp)
+	num**exp
+end
+
+def factorial(num)
+	if num == 0
+		return 1
+	end
+	(1..num).inject(:*)
+end
